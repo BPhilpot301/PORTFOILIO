0255e72e26850422dd64d898341d7fd05a911ebf
BPhilpot301 <britnphilpot43@outlook.com>
2025-11-04 16:01:28 -0600

    second

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from projects import views
+
 urlpatterns = [
+    path("", views.about_me_view, name= "about_me"),
     path('projects/',views.projects_view, name= "projects"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def about_me_view(request):
+    return render(request, 'portfolio/about_me.html')
+
+def experience_view(request):
+    return render(request, 'portfolio/experience.html')
+
+
 def projects_view(request):
     return render(request, 'projects/projects.html')
+
